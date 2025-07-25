29aa35bf60ffc74217cf573144d9597d13ea4bf7
Kabir Shaikh <shaikh.kab@northeastern.edu>
2025-07-25 15:02:38 -0400

    Added helper method in User helper class to map dto to entity and entity to dto. Completed template of User Register logic in service imp

--- src/main/java/com/fitspine/helper/UserHelper.java
+++ src/main/java/com/fitspine/helper/UserHelper.java
@@ -1,8 +1,101 @@
 package com.fitspine.helper;
 
+import com.fitspine.dto.UserDiscIssueDto;
+import com.fitspine.dto.UserInjuryDto;
+import com.fitspine.dto.UserSurgeryDto;
+import com.fitspine.model.User;
+import com.fitspine.model.UserDiscIssue;
+import com.fitspine.model.UserInjury;
+import com.fitspine.model.UserSurgery;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UserHelper {
+    public List<UserInjury> returnUserInjuryList(List<UserInjuryDto> dto, User savedUser) {
+        List<UserInjury> list = new ArrayList<>();
+
+        for (int i = 0; i < dto.size(); i++) {
+            UserInjury userInjury = UserInjury.builder()
+                    .user(savedUser)
+                    .injuryType(dto.get(i).getInjuryType())
+                    .build();
+
+            list.add(userInjury);
+        }
+
+        return list;
+    }
+
+    public List<UserSurgery> returnUserSurgeryList(List<UserSurgeryDto> dto, User savedUser) {
+        List<UserSurgery> list = new ArrayList<>();
+
+        for (int i = 0; i < dto.size(); i++) {
+            UserSurgery userSurgery = UserSurgery.builder()
+                    .user(savedUser)
+                    .surgeryType(dto.get(i).getSurgeryType())
+                    .build();
+
+            list.add(userSurgery);
+        }
+
+        return list;
+    }
+
+    public List<UserDiscIssue> returnUserDiscIssueList(List<UserDiscIssueDto> dto, User savedUser) {
+        List<UserDiscIssue> list = new ArrayList<>();
+
+        for (int i = 0; i < dto.size(); i++) {
+            UserDiscIssue userDiscIssue = UserDiscIssue.builder()
+                    .user(savedUser)
+                    .discLevel(dto.get(i).getDiscLevel())
+                    .build();
+
+            list.add(userDiscIssue);
+        }
+
+        return list;
+    }
+
+    public List<UserInjuryDto> returnMappedUserInjuryListDto(List<UserInjury> userInjuries) {
+        List<UserInjuryDto> list = new ArrayList<>();
+
+        for (int i = 0; i < userInjuries.size(); i++) {
+            UserInjuryDto userInjuryDto = new UserInjuryDto();
+            userInjuryDto.setInjuryType(userInjuries.get(i).getInjuryType());
+
+            list.add(userInjuryDto);
+        }
+
+        return list;
+    }
+
+    public List<UserSurgeryDto> returnMappedUserSurgeryListDto(List<UserSurgery> userSurgeries) {
+        List<UserSurgeryDto> list = new ArrayList<>();
+
+        for (int i = 0; i < userSurgeries.size(); i++) {
+            UserSurgeryDto userSurgeryDto = new UserSurgeryDto();
+            userSurgeryDto.setSurgeryType(userSurgeries.get(i).getSurgeryType());
+
+            list.add(userSurgeryDto);
+        }
+
+        return list;
+    }
+
+    public List<UserDiscIssueDto> returnMappedUserDiscIssueDto(List<UserDiscIssue> userDiscIssues) {
+        List<UserDiscIssueDto> list = new ArrayList<>();
+
+        for (int i = 0; i < userDiscIssues.size(); i++) {
+            UserDiscIssueDto userDiscIssueDto = new UserDiscIssueDto();
+            userDiscIssueDto.setDiscLevel(userDiscIssues.get(i).getDiscLevel());
+
+            list.add(userDiscIssueDto);
+        }
+
+        return list;
+    }
 
 }
--- src/main/java/com/fitspine/service/UserServiceImp.java
+++ src/main/java/com/fitspine/service/UserServiceImp.java
@@ -1,39 +1,87 @@
 package com.fitspine.service;
 
+import com.fitspine.dto.UserInjuryDto;
 import com.fitspine.dto.UserRegisterDto;
 import com.fitspine.dto.UserResponseDto;
 import com.fitspine.helper.UserHelper;
 import com.fitspine.model.User;
+import com.fitspine.model.UserDiscIssue;
+import com.fitspine.model.UserInjury;
+import com.fitspine.model.UserSurgery;
+import com.fitspine.repository.UserDiscIssueRepository;
+import com.fitspine.repository.UserInjuryRepository;
 import com.fitspine.repository.UserRepository;
+import com.fitspine.repository.UserSurgeryRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserServiceImp implements UserService {
     private final UserRepository userRepository;
+    private final UserInjuryRepository userInjuryRepository;
+    private final UserSurgeryRepository userSurgeryRepository;
+    private final UserDiscIssueRepository userDiscIssueRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserHelper userHelper;
 
-    public UserServiceImp(UserRepository userRepository, PasswordEncoder passwordEncoder, UserHelper userHelper) {
+    public UserServiceImp(UserRepository userRepository, UserInjuryRepository userInjuryRepository, UserSurgeryRepository userSurgeryRepository, UserDiscIssueRepository userDiscIssueRepository, PasswordEncoder passwordEncoder, UserHelper userHelper) {
         this.userRepository = userRepository;
+        this.userInjuryRepository = userInjuryRepository;
+        this.userSurgeryRepository = userSurgeryRepository;
+        this.userDiscIssueRepository = userDiscIssueRepository;
         this.passwordEncoder = passwordEncoder;
         this.userHelper = userHelper;
     }
 
     @Override
-    public UserResponseDto registerUser(UserRegisterDto userRegisterDto) {
-        User user = new User();
+    public UserResponseDto registerUser(UserRegisterDto dto) {
+        User user = User.builder()
+                .fullName(dto.getFullName())
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .age(dto.getAge())
+                .gender(dto.getGender())
+                .surgeryHistory(false)
+                .isResearchOpt(true)
+                .isWearableConnected(false)
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        List<UserInjury> userInjuryList = new ArrayList<>();
+        if (dto.getUserInjuries() != null) {
+            userInjuryList = userHelper.returnUserInjuryList(dto.getUserInjuries(), savedUser);
+            userInjuryRepository.saveAll(userInjuryList);
+        }
+
+        List<UserSurgery> userSurgeryList = new ArrayList<>();
+        if (dto.getUserSurgeries() != null) {
+            userSurgeryList = userHelper.returnUserSurgeryList(dto.getUserSurgeries(), savedUser);
+            userSurgeryRepository.saveAll(userSurgeryList);
+        }
+
+        List<UserDiscIssue> userDiscIssueList = new ArrayList<>();
+        if (dto.getUserDiscIssues() != null) {
+            userDiscIssueList = userHelper.returnUserDiscIssueList(dto.getUserDiscIssues(), savedUser);
+            userDiscIssueRepository.saveAll(userDiscIssueList);
+        }
+
 
         return UserResponseDto.builder()
-                .id(user.getId())
-                .fullName(user.getFullName())
-                .email(user.getEmail())
-                .age(user.getAge())
-                .gender(user.getGender())
-                .isResearchOpt(user.getIsResearchOpt())
-                .isWearableConnected(user.getIsWearableConnected())
-                .wearableType(user.getWearableType())
+                .id(savedUser.getId())
+                .fullName(savedUser.getFullName())
+                .email(savedUser.getEmail())
+                .age(savedUser.getAge())
+                .gender(savedUser.getGender())
+                .isResearchOpt(savedUser.getIsResearchOpt())
+                .isWearableConnected(savedUser.getIsWearableConnected())
+                .wearableType(savedUser.getWearableType())
+                .userInjuries(userHelper.returnMappedUserInjuryListDto(userInjuryList))
+                .userSurgeries(userHelper.returnMappedUserSurgeryListDto(userSurgeryList))
+                .userDiscIssues(userHelper.returnMappedUserDiscIssueDto(userDiscIssueList))
                 .build();
     }
-
 }
