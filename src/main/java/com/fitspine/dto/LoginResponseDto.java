3b269b2758c74873f1a13b651b27bcf68ea78544
Kabir Shaikh <shaikh.kab@northeastern.edu>
2025-08-07 14:11:06 -0400

    Added two new dto's for login. Added business logic into the JwtService. Added env config into the ide and in the application properties

---
+++ src/main/java/com/fitspine/dto/LoginResponseDto.java
@@ -0,0 +1,13 @@
+package com.fitspine.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginResponseDto {
+    private String token;
+    private String fullName;
+    private String email;
+    private String profilePicture;
+}
--- src/main/java/com/fitspine/service/JwtService.java
+++ src/main/java/com/fitspine/service/JwtService.java
@@ -1,7 +1,72 @@
 package com.fitspine.service;
 
+import com.fitspine.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.expiration-ms}")
+    private long expirationMs;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        String email = extractEmail(token);
+        if (email.equals(user.getEmail()) && !isTokenExpired(token)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
