a96368336a70dcbafd2a5aa0f1b6a23ecd120d7c
Kabir Shaikh <shaikh.kab@northeastern.edu>
2025-09-22 21:17:49 -0400

    Added new controller to get fitbit api data

---
+++ src/main/java/com/fitspine/controller/FitbitController.java
@@ -0,0 +1,50 @@
+package com.fitspine.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fitspine.service.impl.FitbitApiClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/api/wearable/fitbit")
+public class FitbitController {
+    private final FitbitApiClientService fitbitApiClientService;
+
+    public FitbitController(FitbitApiClientService fitbitApiClientService) {
+        this.fitbitApiClientService = fitbitApiClientService;
+    }
+
+    @GetMapping("/steps")
+    public ResponseEntity<JsonNode> getSteps() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+
+        String date = LocalDate.now().format(DateTimeFormatter.ISO_DATE);
+        return ResponseEntity.ok(fitbitApiClientService.getSteps(email, date));
+    }
+
+    @GetMapping("/sleep")
+    public ResponseEntity<JsonNode> getSleep() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+
+        String date = LocalDate.now().format(DateTimeFormatter.ISO_DATE);
+        return ResponseEntity.ok(fitbitApiClientService.getSleep(email, date));
+    }
+
+    @GetMapping("/heart-rate")
+    public ResponseEntity<JsonNode> getHeartRate() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+
+        String date = LocalDate.now().format(DateTimeFormatter.ISO_DATE);
+        return ResponseEntity.ok(fitbitApiClientService.getHeartRate(email, date));
+    }
+}
