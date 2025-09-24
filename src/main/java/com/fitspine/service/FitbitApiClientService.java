d5038df8d6d3bada1f87d19b13f384e8615590ac
Kabir Shaikh <shaikh.kab@northeastern.edu>
2025-09-24 16:48:07 -0400

    Added logic in fitbit api client service to save activity records

--- src/main/java/com/fitspine/service/FitbitApiClientService.java
+++ src/main/java/com/fitspine/service/FitbitApiClientService.java
@@ -3,14 +3,8 @@ package com.fitspine.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fitspine.client.FitbitApiClient;
 import com.fitspine.exception.UserNotFoundException;
-import com.fitspine.model.FitbitActivitiesHeartLog;
-import com.fitspine.model.FitbitActivitiesHeartValueHeartRateZonesLog;
-import com.fitspine.model.FitbitActivitiesHeartValueLog;
-import com.fitspine.model.User;
-import com.fitspine.repository.FitbitActivitiesHeartLogRepository;
-import com.fitspine.repository.FitbitActivitiesHeartValueHeartRateZonesLogRepository;
-import com.fitspine.repository.FitbitActivitiesHeartValueLogRepository;
-import com.fitspine.repository.UserRepository;
+import com.fitspine.model.*;
+import com.fitspine.repository.*;
 import com.fitspine.service.impl.FitbitServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @Service
@@ -37,20 +34,122 @@ public class FitbitApiClientService {
     private final FitbitActivitiesHeartValueLogRepository fitbitActivitiesHeartValueLogRepository;
 
     private final FitbitActivitiesHeartValueHeartRateZonesLogRepository fitbitActivitiesHeartValueHeartRateZonesLogRepository;
+    private final FitbitActivitiesLogRepository fitbitActivitiesLogRepository;
+    private final FitbitActivitySummariesLogRepository fitbitActivitySummariesLogRepository;
+    private final FitbitActivitySummariesDistancesLogRepository fitbitActivitySummariesDistancesLogRepository;
+    private final FitbitActivityGoalsLogRepository fitbitActivityGoalsLogRepository;
 
-    public FitbitApiClientService(FitbitApiClient fitbitApiClient, UserRepository userRepository, FitbitActivitiesHeartLogRepository fitbitActivitiesHeartLogRepository, FitbitActivitiesHeartValueLogRepository fitbitActivitiesHeartValueLogRepository, FitbitActivitiesHeartValueHeartRateZonesLogRepository fitbitActivitiesHeartValueHeartRateZonesLogRepository, FitbitServiceImpl fitbitService) {
+    public FitbitApiClientService(FitbitApiClient fitbitApiClient,
+                                  UserRepository userRepository,
+                                  FitbitActivitiesHeartLogRepository fitbitActivitiesHeartLogRepository,
+                                  FitbitActivitiesHeartValueLogRepository fitbitActivitiesHeartValueLogRepository,
+                                  FitbitActivitiesHeartValueHeartRateZonesLogRepository fitbitActivitiesHeartValueHeartRateZonesLogRepository,
+                                  FitbitActivitiesLogRepository fitbitActivitiesLogRepository,
+                                  FitbitActivitySummariesLogRepository fitbitActivitySummariesLogRepository,
+                                  FitbitActivitySummariesDistancesLogRepository fitbitActivitySummariesDistancesLogRepository,
+                                  FitbitActivityGoalsLogRepository fitbitActivityGoalsLogRepository,
+                                  FitbitServiceImpl fitbitService) {
         this.fitbitApiClient = fitbitApiClient;
         this.userRepository = userRepository;
         this.fitbitActivitiesHeartLogRepository = fitbitActivitiesHeartLogRepository;
         this.fitbitActivitiesHeartValueLogRepository = fitbitActivitiesHeartValueLogRepository;
         this.fitbitActivitiesHeartValueHeartRateZonesLogRepository = fitbitActivitiesHeartValueHeartRateZonesLogRepository;
+        this.fitbitActivitiesLogRepository = fitbitActivitiesLogRepository;
+        this.fitbitActivitySummariesLogRepository = fitbitActivitySummariesLogRepository;
+        this.fitbitActivitySummariesDistancesLogRepository = fitbitActivitySummariesDistancesLogRepository;
+        this.fitbitActivityGoalsLogRepository = fitbitActivityGoalsLogRepository;
         this.fitbitService = fitbitService;
     }
 
     public JsonNode getSteps(String email, String date) {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User with email: " + email + " not found"));
         log.info("Fetching Fitbit steps for user {} on date {}", user.getId(), date);
-        return fitbitApiClient.getSteps(user.getId(), clientId, clientSecret, date);
+        JsonNode root = fitbitApiClient.getSteps(user.getId(), clientId, clientSecret, date);
+
+        JsonNode activities = root.get("activities");
+        JsonNode summary = root.get("summary");
+        JsonNode goals = root.get("goals");
+
+        if ((activities == null || activities.isEmpty()) && (summary == null || summary.isEmpty()) && (goals == null || goals.isEmpty())) {
+            log.info("No activities data found for user {} on date {}: ", user.getId(), date);
+            return root;
+        }
+
+        //Save Activities:
+        if (activities != null && activities.isArray()) {
+            for (JsonNode activity : activities) {
+                FitbitActivitiesLog activitiesLog = new FitbitActivitiesLog();
+                activitiesLog.setUser(user);
+                activitiesLog.setProvider(fitbitService.getProvider());
+                activitiesLog.setLogDate(LocalDate.parse(date));
+                activitiesLog.setLogId(activity.get("logId").asLong());
+                activitiesLog.setActivityId(activity.get("activityId").asInt());
+                activitiesLog.setActivityParentId(activity.get("activityParentId").asInt());
+                activitiesLog.setActivityParentName(activity.get("activityParentName").asText());
+                activitiesLog.setName(activity.get("name").asText());
+                activitiesLog.setDescription(activity.get("description").asText());
+                activitiesLog.setCalories(activity.get("calories").asInt());
+                activitiesLog.setDistance(activity.get("distance").asDouble());
+                activitiesLog.setSteps(activity.get("steps").asInt());
+                activitiesLog.setDuration(activity.get("duration").asLong());
+                OffsetDateTime lastModified = OffsetDateTime.parse(activity.get("lastModified").asText(), DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+                activitiesLog.setLastModified(lastModified.toLocalDateTime());
+                activitiesLog.setStartTime(LocalTime.parse(activity.get("startTime").asText()));
+                activitiesLog.setFavourite(activity.get("isFavorite").asBoolean());
+                activitiesLog.setHasActiveZoneMinutes(activity.get("hasActiveZoneMinutes").asBoolean());
+                activitiesLog.setStartDate(LocalDate.parse(activity.get("startDate").asText()));
+                activitiesLog.setHasStartTime(activity.get("hasStartTime").asBoolean());
+                fitbitActivitiesLogRepository.save(activitiesLog);
+            }
+        }
+
+        //Save Goals:
+        if (goals != null && goals.isObject()) {
+            FitbitActivityGoalsLog goalsLog = new FitbitActivityGoalsLog();
+            goalsLog.setUser(user);
+            goalsLog.setProvider(fitbitService.getProvider());
+            goalsLog.setLogDate(LocalDate.parse(date));
+            goalsLog.setCaloriesOut(goals.get("caloriesOut").asInt());
+            goalsLog.setSteps(goals.get("steps").asInt());
+            goalsLog.setDistance(goals.get("distance").asDouble());
+            goalsLog.setFloors(goals.get("floors").asInt());
+            goalsLog.setActiveMinutes(goals.get("activeMinutes").asInt());
+
+            fitbitActivityGoalsLogRepository.save(goalsLog);
+        }
+
+        //Save Summary and distances:
+        if (summary != null && summary.isObject()) {
+            FitbitActivitySummariesLog summariesLog = new FitbitActivitySummariesLog();
+            summariesLog.setUser(user);
+            summariesLog.setProvider(fitbitService.getProvider());
+            summariesLog.setLogDate(LocalDate.parse(date));
+            summariesLog.setCaloriesOut(summary.get("caloriesOut").asInt());
+            summariesLog.setActivityCalories(summary.get("activityCalories").asInt());
+            summariesLog.setCaloriesBmr(summary.get("caloriesBMR").asInt());
+            summariesLog.setActiveScore(summary.get("activeScore").asInt());
+            summariesLog.setSteps(summary.get("steps").asInt());
+            summariesLog.setSedentaryMinutes(summary.get("sedentaryMinutes").asInt());
+            summariesLog.setLightlyActiveMinutes(summary.get("lightlyActiveMinutes").asInt());
+            summariesLog.setFairlyActiveMinutes(summary.get("fairlyActiveMinutes").asInt());
+            summariesLog.setVeryActiveMinutes(summary.get("veryActiveMinutes").asInt());
+            summariesLog.setMarginalCalories(summary.get("marginalCalories").asInt());
+            summariesLog.setRawJson(summary.toString());
+            FitbitActivitySummariesLog savedSummary = fitbitActivitySummariesLogRepository.save(summariesLog);
+
+            JsonNode distances = summary.get("distances");
+            if (distances != null && distances.isArray()) {
+                for (JsonNode d : distances) {
+                    FitbitActivitySummariesDistancesLog distancesLog = new FitbitActivitySummariesDistancesLog();
+                    distancesLog.setFitbitActivitySummariesLog(savedSummary);
+                    distancesLog.setActivity(d.get("activity").asText());
+                    distancesLog.setDistance(d.get("distance").asDouble());
+                    fitbitActivitySummariesDistancesLogRepository.save(distancesLog);
+                }
+            }
+        }
+
+        return root;
     }
 
     public JsonNode getSleep(String email, String date) {
